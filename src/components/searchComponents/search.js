81ede8bee2edd5ca2e0608d5ed5d83bcc6fbdbe9
Sam Silver <samsilver@wustl.edu>
2019-06-10 19:41:25 -0700

    end of day 1

---
+++ src/components/searchComponents/search.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const search = (props) => (
+  //Something
+  true
+)
+
+
+const mapStateToProbs = function(state) {
+  return state
+}
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    // increment: () => dispatch({type: 'INCREMENT', payload: 6})
+    // decrement: () => dispatch({type: 'DECREMENT', payload: 5})
+    // reset: () => dispatch({type: 'RESET'})
+  }
+}
+
+
+
+export default connect(mapStateToProps)
