b65269725e9712405ce1aea758a6542f6ba3bfe5
David Levien <davidlevien@Davids-MacBook-Pro.local>
2019-06-12 19:50:55 -0700

    corrected query controller

--- server/controllers/queryController.ts
+++ server/controllers/queryController.ts
@@ -2,16 +2,22 @@ import * as express from 'express';
 import {connectToDB} from '../controllers/connectdb';
 const db: any = connectToDB();
 
+interface Query {
+    text: string;
+    values: any[];
+}
+
 module.exports = {
 
     signUp: (
         req: express.Request, 
         res: express.Response,
         next: express.NextFunction) => {
-        const queryValues = [req.body.firstName, req.body.lastName, req.body.username, req.body.password, req.body.email];
-        const insertQuery = `INSERT INTO user_table("firstName", "lastName", "username", "password", "email") VALUES($1, $2, $3, $4, $5) RETURNING *`;
-            
-        db.query(insertQuery, queryValues, (err, result) => {
+        const queryObj: Query = {
+            text: `INSERT INTO users_table("username", "password", "firstName", "lastName", "email") VALUES($1, $2, $3, $4, $5) RETURNING *`,
+            values: [req.body.firstName, req.body.lastName, req.body.username, req.body.password, req.body.email]
+        };
+        db.query(queryObj, (err, result) => {
           if (err) res.locals.error = err;
           else {
             res.locals.result = result.rows[0];
@@ -26,7 +32,11 @@ module.exports = {
         res: express.Response,
         next: express.NextFunction) => {
         console.log('+++++req.BODY in testSignIn+++++++ ', req.body);
-        db.query(`SELECT * FROM user_table WHERE ("username"='${req.body.username}')`, (err, result) => {
+        const queryObj: Query = {
+            text: "SELECT * FROM users_table WHERE username=$1",
+            values: [req.body.username]
+        }
+        db.query(queryObj, (err, result) => {
           if (err) res.locals.error = err; 
           else {
             res.locals.result = result.rows[0]; // we have access to the hash
@@ -41,7 +51,11 @@ module.exports = {
         req: express.Request, 
         res: express.Response,
         next: express.NextFunction) => {
-        db.query('SELECT * FROM favorites_table', (err, result) => {
+        const queryObj: Query = {
+            text: 'SELECT favorites_table FROM merge_table WHERE user_table = $1',
+            values: [res.locals.userTablePK] //need to store user's primary key on frontend, to send to backend when favorites are requested
+        }
+        db.query(queryObj, (err, result) => {
           if (err) res.locals.error = err;
           else res.locals.result = result;
           return next();
@@ -52,9 +66,11 @@ module.exports = {
         req: express.Request, 
         res: express.Response,
         next: express.NextFunction) => {
-        const queryValues = [req.body.favID];
-        const insertQuery = 'INSERT INTO favorites_table("favID") VALUES($1) RETURNING *';
-        db.query(insertQuery, queryValues, (err, result) => {
+        const queryObj: Query = {
+            text: 'INSERT INTO favorites_table("fav_id") VALUES($1) RETURNING *',
+            values: [req.body.fav_id] //verify fav_id is the same on the front end
+        };
+        db.query(queryObj, (err, result) => {
             if (err) res.locals.error = err;
             else {
                 res.locals.result = result.rows[0];
